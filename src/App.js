f23e9ed58d546ac5bd36f1d9b0701b2d2b65c75e
shahabaz-dev <mshahbazramzan952@gmail.com>
2022-10-03 10:10:49 +0500

    add new component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@
 import logo from './logo.svg';
 import './App.css';
 import React,{useState} from 'react';
+
 function App(){
+
   const [data,setData]=useState(null)
+  const [print,setPrint]=useState(false)
   function getData(val){
     console.warn(val.target.value)
+    setData(val.target.value)
   }
   return(
-    <div className='App'>
-      <h1>{data}</h1>
+    <div className="App">
+      {
+        print?
+        <h1>{data}</h1>
+        :null
+        
+      }
       <input type="text" onChange={getData} />
+      <button onClick={()=>setPrint(true)}>Print Data</button>
 
     </div>
   )
